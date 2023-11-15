ffe24c586549093c114c9e01e77c32642a10f73f
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-11-15 17:50:51 -0300

    feat: creating entities GameList and Belonging

---
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gameListId;
+    private String name;
+
+    public GameList() {}
+
+    public GameList(Long gameListId, String name) {
+        this.gameListId = gameListId;
+        this.name = name;
+    }
+
+    public Long getGameListId() {
+        return gameListId;
+    }
+
+    public void setGameListId(Long gameListId) {
+        this.gameListId = gameListId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(gameListId, gameList.gameListId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameListId);
+    }
+}
