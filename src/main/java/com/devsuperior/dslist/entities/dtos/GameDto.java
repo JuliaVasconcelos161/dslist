dc559d9e13fa715e7fc3ff81333a74bf2510f019
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-11-15 18:06:29 -0300

    feat: creating GameDto

---
+++ src/main/java/com/devsuperior/dslist/entities/dtos/GameDto.java
@@ -0,0 +1,94 @@
+package com.devsuperior.dslist.entities.dtos;
+
+import com.devsuperior.dslist.entities.Game;
+import org.springframework.beans.BeanUtils;
+
+public class GameDto {
+    private Integer gameId;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String urlImg;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDto(){}
+
+    public GameDto(Game entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Integer getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(Integer gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+}
